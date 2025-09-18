c1f0c79735d7bbe1c5d9ac015bab733665e8cdd8
NallelyPB <nallelyparedes6@gmail.com>
2025-09-18 18:32:45 -0500

    Modificacion de clase madre y clase hija

--- src/Cuenta.kt
+++ src/Cuenta.kt
@@ -1,2 +1,32 @@
-class Cuenta {
+open class Cuenta(var saldo: Float, var tanual: Float) {
+    var nconsignaciones: Int = 0
+    var nretiros: Int = 0
+    var comisionmensual: Float =0.0f
+    open fun consignar(cantidad: Float) {
+        saldo += cantidad
+        nconsignaciones++
+    }
+    open fun retirar(cantidad: Float) {
+        if (cantidad <= saldo) {
+            saldo -= cantidad
+            nretiros++
+        } else {
+            println("Fondos insuficientes.")
+        }
+    }
+    open fun calcularInteresMensual() {
+        val interesMensual = saldo * (tanual / 12)
+        saldo += interesMensual
+    }
+    open fun extractoMensual() {
+        saldo -= comisionmensual
+        calcularInteresMensual()
+    }
+    open fun imprimir() {
+        println("Saldo: $saldo")
+        println("Número de consignaciones: $nconsignaciones")
+        println("Número de retiros: $nretiros")
+        println("Tasa anual: $tanual%")
+        println("Comisión mensual: $comisionmensual")
+    }
 }
