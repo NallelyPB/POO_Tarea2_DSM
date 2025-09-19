10f64ed11757060fd275070259f7619b4b33492c
Angela <angela.celestino@unmsm.edu.pe>
2025-09-18 19:55:24 -0500

    Carga de Cuenta contable y el main

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,24 @@
 fun main() {
-    val cuenta = Cuenta(1000f, 0.3f)
+    //Prueba
+    /*val cuenta = Cuenta(1000f, 0.3f)
     println("Saldo inicial: ${cuenta.saldo}")
     cuenta.extractoMensual()
-    println("Saldo después del extracto mensual: ${cuenta.saldo}")
+    println("Saldo después del extracto mensual: ${cuenta.saldo}")*/
+    // Cuenta de Ahorros
+    val cuentaAhorros = Cuenta_Ahorro(15000f, 12f)
+    cuentaAhorros.retirar(2000f)
+    cuentaAhorros.consignar(1000f)
+    cuentaAhorros.retirar(500f)
+    cuentaAhorros.retirar(300f)
+    cuentaAhorros.retirar(200f)
+    cuentaAhorros.retirar(100f) // genera comisión extra
+    cuentaAhorros.extractoMensual()
+    cuentaAhorros.imprimir()
+    println()
+    // Cuenta Corriente
+    val cuentaCorriente = Cuenta_Corriente(5000f, 12f)
+    cuentaCorriente.retirar(7000f) // genera sobregiro
+    cuentaCorriente.consignar(2000f) // reduce sobregiro
+    cuentaCorriente.extractoMensual()
+    cuentaCorriente.imprimir()
 }
